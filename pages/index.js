dfffae8467602a2fd362591f19a338a53117837a
Ansal Sajan <ansalsajan.eee@gmail.com>
2024-06-17 20:20:36 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello, <a href="#">World!</a>
+          Hello Sajan, <a href="#">World!</a>
         </h1>
         <p className={styles.description}>
           Welcome to your new Next.js application.
